ee5bc639b24a063bbef229a11ae80d4bf5af39f7
prasannakumar2495 <prasannakumar2461995@gmail.com>
2023-10-09 21:10:26 +0530

    Not able to resolve the issue with name tag due to gradle version and android studio version.

--- app/src/main/java/com/pk/navgraphxmljetpack/MainActivity.kt
+++ app/src/main/java/com/pk/navgraphxmljetpack/MainActivity.kt
@@ -8,6 +8,8 @@ import com.pk.navgraphxmljetpack.databinding.ActivityMainBinding
 
 /**
  * https://stackoverflow.com/questions/76796254/android-navigation-define-argument
+ * https://developer.android.com/build/releases/gradle-plugin
+ * https://developer.android.com/studio/releases#android_gradle_plugin_and_android_studio_compatibility
  */
 
 class MainActivity : AppCompatActivity() {
